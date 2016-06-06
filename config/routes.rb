aa5fd8d1f76566cbe8842ea31565a0b3e8748806
Shahrooz <shahrooz84@gmail.com>
2016-06-06 14:58:30 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+
+
+  root 'users#index'
+  resources :users
+  resources :sessions, only: [:new, :create]
+
+  delete '/logout' => 'sessions#destroy', as: :logout
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+end
